OpenMandrivaAssociation/valgrind

commit 03b43120f43c06db8b3ed0ab3b590725db711e2d

version update 3.8.0 synced with mageia and fedora
SILENT: svn-revision: 815875

valgrind.spec
@@ -1,45 +1,48 @@
-%define	_filter_GLIBC_PRIVATE	1
+%define _requires_exceptions GLIBC_PRIVATE
 
 #one of valgrind internal library is not linked to libc. Not sure if it should be fixed or not (since all test don't pass anyway), so disable check for now
 %define _disable_ld_no_undefined 1
 
-Name:		valgrind
-Version:	3.7.0
-Release:	3
+# disable debug package, it can prevent valgrind for working properly
+%define debug_package   %{nil}
+
+# (blino) FIXME: reenable qt4 as soon as availble in cauldron
+%bcond_without	qt4
+
+Name: 		valgrind
+Version:	3.8.0
+Release:	%mkrel 1
 Summary: 	Memory debugger
 License: 	GPLv2+
 Group: 		Development/Other
-# a855fda56edf05614f099dca316d1775
 Source0:	http://www.valgrind.org/downloads/%{name}-%{version}.tar.bz2
-Patch1:		valgrind-3.7.0-cachegrind-improvements.patch
-Patch2:		valgrind-3.7.0-openat.patch
-Patch3:		valgrind-3.7.0-helgrind-race-supp.patch
-Patch4:		valgrind-3.7.0-stat_h.patch
-Patch5:		valgrind-3.7.0-capget.patch
-Patch6:		valgrind-3.7.0-pie.patch
-Patch7:		valgrind-3.7.0-gcc-version.patch
-# Do not tell ebx is clobbered in exit syscall
-# to avoid gcc error, as done in valgrind 3.6.1
-Patch8:		valgrind-3.7.0-pic-clobber.patch
-Patch9:		valgrind-3.7.0-glibc-2.15.patch
-Patch10:	valgrind-3.7.0-tests.patch
-Patch11:	valgrind-3.7.0-f-sgetown-ex.patch
-Patch12:	valgrind-3.7.0-scsi-ioctls.patch
-Patch13:	valgrind-3.7.0-enable-armv5.patch
-Patch14:	valgrind-3.7.0-ldso-supp.patch
-Patch15:	valgrind-3.7.0-unspecified-type.patch
-Patch16:	valgrind-3.7.0-rvalue-ref.patch
-Patch17:	valgrind-3.7.0-debug-leak1.patch
-Patch18:	valgrind-3.7.0-debug-leak2.patch
-Patch19:	valgrind-3.7.0-addToXA.patch
-Patch20:	valgrind-3.7.0-debug-types.patch
+# From Fedora:
+Patch1:		valgrind-3.8.0-cachegrind-improvements.patch
+Patch2:		valgrind-3.8.0-openat.patch
+Patch3:		valgrind-3.8.0-helgrind-race-supp.patch
+Patch4:		valgrind-3.8.0-stat_h.patch
+Patch5:		valgrind-3.8.0-capget.patch
+Patch6:		valgrind-3.8.0-pie.patch
+Patch7:		valgrind-3.8.0-config_h.patch
+Patch8:		valgrind-3.8.0-tests.patch
+Patch9:		valgrind-3.8.0-enable-armv5.patch
+Patch10:	valgrind-3.8.0-ldso-supp.patch
+Patch11:	valgrind-3.8.0-x86-backtrace.patch
+Patch12:	valgrind-3.8.0-find-buildid.patch
+Patch13:	valgrind-3.8.0-abbrev-parsing.patch
+
+# https://bugs.kde.org/show_bug.cgi?id=305513
+Patch102:	valgrind-fix-segv.diff
 
 URL: 		http://valgrind.org/
-ExclusiveArch:	%{ix86} x86_64 ppc %{arm}
+ExclusiveArch:	%{ix86} x86_64 ppc
 BuildRequires:	glibc-static-devel
 BuildRequires:	gdb
 # (proyvind): build with support for OpenMP, boost & qt4 threads
-BuildRequires:	gomp-devel boost-devel qt4-devel
+BuildRequires:	libgomp-devel boost-devel
+%if %{with qt4}
+BuildRequires:	qt4-devel
+%endif
 Suggests:	gdb
 Obsoletes:	valgrind-plugins
 
@@ -56,35 +59,79 @@ intercepted. As a result, Valgrind can detect problems such as:
     * Passing of uninitialised and/or unaddressible memory to system calls
     * Mismatched use of malloc/new/new [] vs free/delete/delete []
 
+%files
+%doc README* AUTHORS FAQ.txt
+%{_bindir}/*
+%{_libdir}/%{name}
+%{_mandir}/man1/*.1*
+%{_datadir}/valgrind/*.xml
+%{_datadir}/valgrind/default.supp
+
+#--------------------------------------------------------------------
+
 %package	devel
 Summary:	%{summary}
 Group:		%{group}
+Conflicts:	%{name} < 3.6.1-3
 
 %description	devel
 Development files required to develop software using valgrind.
 
+%files devel
+%dir %{_includedir}/valgrind
+%{_includedir}/valgrind/*
+%{_libdir}/pkgconfig/%{name}.pc
+
+#--------------------------------------------------------------------
+
 %prep
-%setup -q 
+%setup -q
 %apply_patches
 
-# not just a simple case of s|pkglib_|pkgdata_|g; in Makefile.am files
-# with newer autoconf
-%if 0
-# required for qt4 thread support as configure script shipped with package were
-# generated with either outdated or missing pkg-config
-autoreconf
-%endif
-
 %build
-%configure
+
+# Convert the library paths with /lib or /lib64 in the suppressions to those
+# with /usr/lib or /usr/lib64 due to the /usrmove .
+perl -pi -e 's!obj:/lib!obj:/usr/lib!g' *.supp *.supp.in
+
+# (From Fedora):
+# Filter out some flags that cause lots of valgrind test failures.
+# Also filter away -O2, valgrind adds it wherever suitable, but
+# not for tests which should be -O0, as they aren't meant to be
+# compiled with -O2 unless explicitely requested.
+export CFLAGS="`echo " %{optflags} " | sed 's/ -m\(64\|3[21]\) / /g;s/ -fexceptions / /g;s/ -fstack-protector / / g;s/ -Wp,-D_FORTIFY_SOURCE=2 / /g;s/-O2 / /g;s/^ //;s/ $//'`"
+
+# filter out one more flag (unused in FC but causes faillure in mga3:
+export CFLAGS="`echo " ${CFLAGS} " | sed -e 's/ -fPIC//'`"
+# fix flags in other cases (CXXFLAGS, FFLAGS):
+%define optflags $CFLAGS
+
+# rebuild unconditionally because we patch configure.in
+autoreconf
+%configure2_5x
+
 %make
 
 %install
-export EXCLUDE_FROM_STRIP=%{_libdir}/valgrind/*.so
+# Don't strip (prevent valgrind from working properly, as explained in README_PACKAGERS)
+export DONT_STRIP=1
+
+# FIXME exporting DONT_STRIP=1 is causing some weird behavior with debug_package
+# and causing it to not set DISABLE_DEBUG=1
+export DISABLE_DEBUG=1
+
 %makeinstall
 
+mkdir %buildroot/%_datadir/valgrind
+mv  %buildroot/%_libdir/valgrind/*.{supp,xml} %buildroot/%_datadir/valgrind
+
+# It's a kludge, but appears to get valgrind to work, because it looks for
+# default.supp under %%{_libdir} instead of under %%{_datadir}, where it is
+# now installed.
+ln -sf %{_datadir}/valgrind/default.supp "$RPM_BUILD_ROOT"/%{_libdir}/valgrind/default.supp
+
 #don't package generated files
-rm -f %{buildroot}%{_libdir}/valgrind/*.supp.in
+rm -f $RPM_BUILD_ROOT%{_libdir}/valgrind/*.supp.in
 
 %check
 # Ensure there are no unexpected file descriptors open,
@@ -112,24 +159,12 @@ for i in `find . -type f \( -name *-amd64-linux -o -name *-x86-linux -o -name *-
   esac
 done
 
-# Some tests have issues with non volatile global variables accessed
-# by multiple threads on i586, and also in i586, some tests fail due
-# to not being able to modify running testcase variables in the
-# gdb server interface, and then looping forever e.g. sleepers test.
-make CFLAGS="-O0 -g3" CXXFLAGS="-O0 -g3" check || :
+make check || :
 # some tests are known to fail,
 # just check no more tests are failing than Fedora ;)
 echo ===============TESTING===================
-./close_fds make regtest || :
+# Cancel this temporarily because the tests hang on x86-64:
+# http://thread.gmane.org/gmane.comp.debugging.valgrind/11792
+# ./close_fds make regtest || :
 echo ===============END TESTING===============
 
-%files
-%doc README* AUTHORS FAQ.txt
-%{_bindir}/*
-%{_libdir}/%{name}
-%{_mandir}/man1/*.1*
-
-%files devel
-%dir %{_includedir}/valgrind
-%{_includedir}/valgrind/*
-%{_libdir}/pkgconfig/%{name}.pc

commit b06910a8a4c5200c4fc2d69612e731e661276c07

fix so that we again properly strip only the relevant files (got broken in 03b43120f43c06db8b3ed0ab3b590725db711e2d), which also caused difficulties debugging code using C++ STL

valgrind.rpmlintrc
@@ -1,3 +1,3 @@
 addFilter('valgrind\..* shared-lib-without-dependency-information.*/usr/lib.*/valgrind/vgpreload.*\.so')
-addFilter('valgrind\..* statically-linked-binary.* /usr/lib64/valgrind/*-linux')
-
+addFilter('valgrind\..* statically-linked-binary.* /usr/lib.*/valgrind/.*-linux')
+addFilter('valgrind\..* unstripped-binary-or-object.* /usr/lib.*/valgrind/.*')

valgrind.spec
@@ -1,14 +1,11 @@
 #one of valgrind internal library is not linked to libc. Not sure if it should be fixed or not (since all test don't pass anyway), so disable check for now
 %define _disable_ld_no_undefined 1
 
-# disable debug package, it can prevent valgrind for working properly
-%define	debug_package	%{nil}
-
 %bcond_without	qt4
 
 Name:		valgrind
 Version:	3.9.0
-Release:	2
+Release:	3
 Summary:	Memory debugger
 License:	GPLv2+
 Group:		Development/Other
@@ -173,12 +170,7 @@ export CFLAGS="`echo " ${CFLAGS} " | sed -e 's/ -fPIC//'`"
 %make
 
 %install
-# Don't strip (prevent valgrind from working properly, as explained in README_PACKAGERS)
-export DONT_STRIP=1
-
-# FIXME exporting DONT_STRIP=1 is causing some weird behavior with debug_package
-# and causing it to not set DISABLE_DEBUG=1
-export DISABLE_DEBUG=1
+export EXCLUDE_FROM_STRIP=%{_libdir}/valgrind/
 
 %makeinstall
 